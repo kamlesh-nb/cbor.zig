db24698427652d20527b16901912ac14ede19876
kamlesh-nb <kamlesh.bambarde@outlook.com>
2025-07-27 16:04:56 +0530

    ...

--- src/root.zig
+++ src/root.zig
@@ -6,6 +6,7 @@ pub const CborError = cbor.CborError;
 pub const Config = cbor.Config;
 pub const Encoder = cbor.Encoder;
 pub const Decoder = cbor.Decoder;
+pub const CBOR = cbor.CBOR;
 
 // Additional exports
 pub const encodeEnum = cbor.encodeEnum;
